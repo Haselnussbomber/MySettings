42290764f7ca612255bf28cb20253354e75e8687
Haselnussbomber <mail@haselnussbomber.de>
2016-10-16 04:15:55 +0200

    let me zoom out in monk order hall

--- WorldMap.lua
+++ WorldMap.lua
@@ -4,3 +4,9 @@ end
 
 hooksecurefunc("WorldMap_ToggleSizeUp", resize)
 hooksecurefunc("WorldMap_ToggleSizeDown", resize)
+
+hooksecurefunc("WorldMapZoomOutButton_OnClick", function()
+	if (GetCurrentMapAreaID() == 1044) then
+		SetMapZoom(WORLDMAP_AZEROTH_ID);
+	end
+end)
