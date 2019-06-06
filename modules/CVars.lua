6b83333c2945adbc894e50be24fc230c93a40a6f
Haselnussbomber <mail@haselnussbomber.de>
2019-06-07 00:56:55 +0200

    rework CVars module

--- modules/CVars.lua
+++ modules/CVars.lua
@@ -1,108 +1,101 @@
 local _, addon = ...
 
-local GetCVar = GetCVar
-local SetCVar = SetCVar
-local GetCVarBitfield = GetCVarBitfield
-local SetCVarBitfield = SetCVarBitfield
+local SetCVar = SetCVar -- from SharedXML/Util.lua
+local GetCVarBitfield = C_CVar.GetCVarBitfield
+local SetCVarBitfield = C_CVar.SetCVarBitfield
 
 local Module = {
 	name = "cvars",
 	events = { "PLAYER_ENTERING_WORLD", "ADDON_LOADED" }
 }
 
-local cvars = {
-	-- Camera
-	cameraDistanceMaxZoomFactor = "3.4", -- https://www.wowinterface.com/downloads/info24927-MaxCamClassic.html
-	cameraPitchMoveSpeed = "45",
-	cameraPitchSmoothSpeed = "45",
-	cameraSmoothStyle = "0",
-	cameraSmoothTrackingStyle = "2",
-	cameraYawMoveSpeed = "90",
-	cameraYawSmoothSpeed = "170",
-
-	-- Interface
-	alwaysCompareItems = "1",
-	autoLootDefault = "1",
-	deselectOnClick = "1",
-	enableFloatingCombatText = "1",
-	interactOnLeftClick = "0",
-	nameplateShowAll = "1",
-
-	-- Chat
-	chatBubblesParty = "0",
-	profanityFilter = "0",
-	removeChatDelay = "1", -- disable hover delay
-
-	-- Tutorials
-	addFriendInfoShown = 1,
-	pendingInviteInfoShown = 1,
-	showTokenFrame = 1,
-	showTutorials = 0,
-	talentFrameShown = 1,
-
-	-- Graphics
-	componentTextureLevel = "0",
-	--outlineEngineMode = "2", -- what does it do?
-	projectedTextures = "1",
-	--RAIDoutlineEngineMode = "2", -- what does it do?
-
-	-- Sound
-	Sound_EnableErrorSpeech = "0",
-	Sound_EnableMusic = "0",
-	Sound_EnableSoundWhenGameIsInBG = "1",
-}
-
-if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
-	-- Interface
-	cvars.autoDismountFlying = "1";
-	cvars.nameplateShowSelf = "0";
-
-	-- Sound
-	cvars.Sound_EnableDSPEffects = "0";
-
-	-- Tutorials
-	cvars.showNPETutorials = 0;
-
-	-- Tutorials from AddOns\Blizzard_VoidStorageUI\Blizzard_VoidStorageUI.lua (#voidStorageTutorials)
-	cvars.lastVoidStorageTutorial = 3;
-
-	-- Tutorials from AddOns\Blizzard_GarrisonUI\Blizzard_OrderHallMissionUI.lua (seenAllTutorials)
-	cvars.orderHallMissionTutorial = 0x000F0004;
-
-	-- Tutorials from AddOns\Blizzard_GarrisonUI\Blizzard_GarrisonShipyardUI.lua
-	cvars.shipyardMissionTutorialFirst = 1;
-	cvars.shipyardMissionTutorialBlockade = 1;
-	cvars.shipyardMissionTutorialAreaBuff = 1;
-	cvars.dangerousShipyardMissionWarningAlreadyShown = 1;
-else
-	cvars.instantQuestText = 1;
-	cvars.nameplateMaxDistance = "80";
-
-	-- Action Bars (uvars)
-	SHOW_MULTI_ACTIONBAR_1 = 1;
-	SHOW_MULTI_ACTIONBAR_2 = 1;
-	SHOW_MULTI_ACTIONBAR_3 = 1;
-	SHOW_MULTI_ACTIONBAR_4 = 1;
-	ALWAYS_SHOW_MULTIBARS = 1;
-	InterfaceOptions_UpdateMultiActionBars();
-end
-
 function Module:PLAYER_ENTERING_WORLD()
     Module.PLAYER_ENTERING_WORLD = nil
 
-	for cvar, value in pairs(cvars) do
-		local current = tostring(GetCVar(cvar))
-		if (current ~= value) then
-			SetCVar(cvar, value)
-		end
-	end
+	-- see https://wow.gamepedia.com/Console_variables/Complete_list
+	-- cameraDistanceMaxZoomFactor calculation by https://www.wowinterface.com/downloads/info24927-MaxCamClassic.html
+
+	-- Camera
+	SetCVar("cameraPitchMoveSpeed", 45)
+	SetCVar("cameraSmoothStyle", 0)         -- Never adjust camera.
+	SetCVar("cameraSmoothTrackingStyle", 0) -- Never adjust camera. (for click to move)
+	SetCVar("cameraYawMoveSpeed", 90)
+
+	-- Interface
+	SetCVar("alwaysCompareItems", true)
+	SetCVar("autoLootDefault", true)
+	SetCVar("deselectOnClick", true)
+	SetCVar("enableFloatingCombatText", true)
+	SetCVar("interactOnLeftClick", false)
+	SetCVar("nameplateShowAll", true)
+
+	-- Chat
+	SetCVar("chatBubblesParty", false)
+	SetCVar("profanityFilter", false)
+
+	-- Tutorials
+	SetCVar("addFriendInfoShown", true)
+	SetCVar("pendingInviteInfoShown", true)
+	SetCVar("showTokenFrame", true)
+	SetCVar("showTutorials", false)
+	SetCVar("talentFrameShown", true)
+
+	-- Graphics
+	SetCVar("componentTextureLevel", 0) -- higher resolution for gear
+	SetCVar("projectedTextures", true)
+	--[[
+	SetCVar("outlineEngineMode", 2)     -- TODO: 2? default is 0 - off. weird
+	SetCVar("RAIDoutlineEngineMode", 2) -- TODO: 2? default is 0 - off. weird
+	--]]
+
+	-- Sound
+	SetCVar("Sound_EnableErrorSpeech", false)
+	SetCVar("Sound_EnableMusic", false)
+	SetCVar("Sound_EnableSoundWhenGameIsInBG", true)
 
 	if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+		-- Camera
+		SetCVar("cameraDistanceMaxZoomFactor", 39 / 15)
+
+		-- Interface
+		SetCVar("autoDismountFlying", true)
+		SetCVar("nameplateShowSelf", false) -- class ressource bar
+
+		-- Tutorials
+		SetCVar("showNPETutorials", false)
+
+		-- Tutorials from AddOns\Blizzard_VoidStorageUI\Blizzard_VoidStorageUI.lua (#voidStorageTutorials)
+		SetCVar("lastVoidStorageTutorial", 3)
+
+		-- Tutorials from AddOns\Blizzard_GarrisonUI\Blizzard_OrderHallMissionUI.lua (seenAllTutorials)
+		SetCVar("orderHallMissionTutorial", "0x000F0004")
+
+		-- Tutorials from AddOns\Blizzard_GarrisonUI\Blizzard_GarrisonShipyardUI.lua
+		SetCVar("shipyardMissionTutorialFirst", true)
+		SetCVar("shipyardMissionTutorialBlockade", true)
+		SetCVar("shipyardMissionTutorialAreaBuff", true)
+		SetCVar("dangerousShipyardMissionWarningAlreadyShown", true)
+
+		-- more Tutorials
 		for key, value in pairs(_G) do
 			if (string.sub(key, 0, 18) == "LE_FRAME_TUTORIAL_" and not GetCVarBitfield("closedInfoFrames", value)) then
 				SetCVarBitfield("closedInfoFrames", value, true)
 			end
 		end
+	else -- CLASSIC
+		-- Camera
+		SetCVar("cameraDistanceMaxZoomFactor", 50 / 15)
+
+		-- Interface
+		SetCVar("instantQuestText", true)
+
+		-- Action Bars (uvars)
+		SHOW_MULTI_ACTIONBAR_1 = 1
+		SHOW_MULTI_ACTIONBAR_2 = 1
+		SHOW_MULTI_ACTIONBAR_3 = 1
+		SHOW_MULTI_ACTIONBAR_4 = 1
+		ALWAYS_SHOW_MULTIBARS = 1
+		InterfaceOptions_UpdateMultiActionBars()
 	end
 end
 
