98f1a28ddee266d71ba7c2b0aa2508ecfdfed6bf
Haselnussbomber <mail@haselnussbomber.de>
2019-08-12 05:15:40 +0200

    add QuestReward
    automatically selects reward with the highest sell price

---
+++ modules/QuestReward.lua
@@ -0,0 +1,27 @@
+local _, addon = ...
+
+local module = addon:RegisterModule("QuestReward")
+module:RegisterEvent("QUEST_COMPLETE")
+
+function module:QUEST_COMPLETE()
+	-- default first button when no item has a sell value.
+	local choice, price = 1, 0
+	local num = GetNumQuestChoices()
+
+	if num <= 0 then
+		return	-- no choices, quick exit
+	end
+
+	for index = 1, num do
+		local link = GetQuestItemLink("choice", index);
+		if (link) then
+			local itemSellPrice = select(11, GetItemInfo(link))
+			if itemSellPrice and itemSellPrice > price then
+				price = itemSellPrice
+				choice = index
+			end
+		end
+	end
+
+    QuestInfoItem_OnClick(QuestInfo_GetRewardButton(QuestInfoFrame.rewardsFrame, choice))
+end
