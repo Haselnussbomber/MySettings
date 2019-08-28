969f5683d076b901b286a990d183831629aa0f7f
Haselnussbomber <mail@haselnussbomber.de>
2019-08-28 03:07:16 +0200

    add ChatSkillLevel for classic

---
+++ modules/classic/ChatSkillLevel.lua
@@ -0,0 +1,36 @@
+if (WOW_PROJECT_ID ~= WOW_PROJECT_CLASSIC) then
+    return
+end
+
+local function ConvertFormat(fmt)
+   -- remove non-English format params like "%1$s"
+   local find = string.gsub(fmt, "%%%d%$", "%%")
+
+   -- remove any special characters with escaped versions
+   find = string.gsub(find, "([%^%$%(%)%.%[%]%*%+%-%?])", "%%%1")
+
+   -- finally replace standard "%s" and "%d"
+   find = string.gsub(find, "%%s", "(.+)")
+   find = string.gsub(find, "%%d", "(%%d+)")
+   return find
+end
+
+local pattern = ConvertFormat(ERR_SKILL_UP_SI)
+
+local function filter(_, _, msg, ...)
+	local start, _, prof, rank = string.find(msg, pattern)
+
+	if start then
+		local numSkills = GetNumSkillLines();
+		for i=1, numSkills do
+			local skillName, _, _, _, _, _, skillMaxRank = GetSkillLineInfo(i);
+			if skillName == prof then
+				msg = string.gsub(msg, rank, rank.."/"..skillMaxRank)
+			end
+		end
+	end
+
+	return false, msg, ...
+end
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_SKILL", filter)
