ed940e21bf2f7e0837fb2915deaa9835a5f0e0ac
Haselnussbomber <mail@haselnussbomber.de>
2020-10-24 01:26:17 +0200

    minor code style fixes

--- modules/shared/Tooltip/Units.lua
+++ modules/shared/Tooltip/Units.lua
@@ -164,8 +164,8 @@ local function OnTooltipSetUnit(self)
 			local tbl = {};
 
 			-- name
-			local playerFlag = ""
-			local mentorshipStatus = C_PlayerMentorship.GetMentorshipStatus(PlayerLocation:CreateFromUnit(unit))
+			local playerFlag = "";
+			local mentorshipStatus = C_PlayerMentorship.GetMentorshipStatus(PlayerLocation:CreateFromUnit(unit));
 			if (mentorshipStatus == Enum.PlayerMentorshipStatus.Mentor) then
 				playerFlag = "|A:newplayerchat-chaticon-guide:0:0:0:0|a "; -- NPEV2_CHAT_USER_TAG_GUIDE
 			elseif (mentorshipStatus == Enum.PlayerMentorshipStatus.Newcomer) then
