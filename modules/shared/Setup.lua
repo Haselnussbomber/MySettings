29a20cf8b88fc44e0b3d5d6351ad1b5d9cf3b4a2
Haselnussbomber <mail@haselnussbomber.de>
2020-10-24 01:25:12 +0200

    setup: set only if AutoTurnIn is loaded

--- modules/shared/Setup.lua
+++ modules/shared/Setup.lua
@@ -1,8 +1,10 @@
 SLASH_SETUP1 = "/setup"
 SlashCmdList["SETUP"] = function()
 	-- AutoTurnIn
+	if (AutoTurnInCharacterDB) then
 	AutoTurnInCharacterDB["all"] = 3
 	AutoTurnInCharacterDB["showrewardtext"] = false
+	end
 
 	-- Kui Nameplates
 	KuiNameplatesCoreCharacterSaved["profile"] = "MyProfile"
