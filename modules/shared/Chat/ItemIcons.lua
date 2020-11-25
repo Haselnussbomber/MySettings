4f92b3f294e6917a193bb798c91d97ccea5e6da8
Haselnussbomber <mail@haselnussbomber.de>
2020-11-25 03:49:48 +0100

    chat: add textures to mawpower links

--- modules/shared/Chat/ItemIcons.lua
+++ modules/shared/Chat/ItemIcons.lua
@@ -1,10 +1,12 @@
+local _, addon = ...
+
 local match = string.match
 local gsub = string.gsub
 
 local textureFormat = "|T%s:12|t"
 
 local handlers = {
-	["(|?%x*|Hitem:(.-)|h.-|h|?r?)"] = function(link, linkData)
+	["(|c%x+|Hitem:(.-)|h.-|h|?r)"] = function(link, linkData)
 		local itemId = match(linkData, "^%d+")
 		local texture = GetItemIcon(itemId)
 		if (texture) then
@@ -13,7 +15,7 @@ local handlers = {
 		return link
 	end,
 
-	["(|?%x*|Hspell:(.-)|h.-|h|?r?)"] = function(link, linkData)
+	["(|c%x+|Hspell:(.-)|h.-|h|?r)"] = function(link, linkData)
 		local spellId = match(linkData, "^%d+")
 		local texture = select(3, GetSpellInfo(spellId))
 		if (texture) then
@@ -24,7 +26,7 @@ local handlers = {
 }
 
 if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
-	handlers["(|?%x*|Hachievement:(.-)|h.-|h|?r?)"] = function(link, linkData)
+	handlers["(|c%x+|Hachievement:(.-)|h.-|h|?r)"] = function(link, linkData)
 		local achievementId = match(linkData, "^%d+")
 		local texture = select(10, GetAchievementInfo(achievementId))
 		if (texture) then
@@ -33,7 +35,7 @@ if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
 		return link
 	end
 
-	handlers["(|?%x*|Hbattlepet:(.-)|h.-|h|?r?)"] = function(link)
+	handlers["(|c%x+|Hbattlepet:(.-)|h.-|h|?r)"] = function(link)
 		-- copied from Interface/FrameXML/DressUpFrames.lua (8.0.1)
 		local _, _, _, _, speciesIDString, _, _, _, _, _, battlePetID = strsplit(":|H", link)
 		local speciesID, _, _, _, _, _, _, _, texture = C_PetJournal.GetPetInfoByPetID(battlePetID)
@@ -48,11 +50,23 @@ if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
 		return link
 	end
 
-	handlers["(|?%x*|Hcurrency:(.-)|h.-|h|?r?)"] = function(link, linkData)
+	handlers["(|c%x+|Hcurrency:(.-)|h.-|h|?r)"] = function(link, linkData)
 		local currencyId = match(linkData, "^%d+")
 		local texture = C_CurrencyInfo.GetCurrencyInfo(currencyId).iconFileID
 		if (texture) then
 			return textureFormat:format(texture) .. link
+		end
+		return link
+	end
+
+	-- |cff71d5ff|Hmawpower:1177|h[Strahlende Essenz]|h|r
+	handlers["(|c%x+|Hmawpower:(%d+)|h.-|h|r)"] = function(link, id)
+		local spellID = addon.GetMawPowerSpellID(id)
+		if (spellID) then
+			local texture = select(3, GetSpellInfo(spellID))
+			if (texture) then
+				return textureFormat:format(texture) .. link
+			end
 		end
 		return link
 	end
