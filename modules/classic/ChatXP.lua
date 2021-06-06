65eebc708cd9cc45dca319133ea0f771a2b1c25f
Haselnussbomber <mail@haselnussbomber.de>
2021-06-06 20:19:00 +0200

    rework ChatXP

--- modules/classic/ChatXP.lua
+++ modules/classic/ChatXP.lua
@@ -1,27 +1,37 @@
-local curXP = UnitXP("player");
-local curMaxXP = UnitXPMax("player");
-local remaining = 0;
-local function calcxpgain(new)
-	if (new > curXP )then
-		local d = new-curXP;
-		curXP = new;
-		return d;
-	else
-		local d = curMaxXP-curXP+new;
-		curXP = new;
-		curMaxXP = UnitXPMax("player");
-		return d;
-	end
-end
+local cur = 0;
+local max = 0;
 
 local frame = CreateFrame("Frame");
-frame:RegisterEvent("PLAYER_XP_UPDATE");
-frame:SetScript("OnEvent", function(self, event, ...)
-	local xp = UnitXP("player");
-	local xpMax = UnitXPMax("player");
-	local gained = calcxpgain(xp);
-	remaining = xpMax-xp;
-	local mobs = math.ceil(remaining/gained);
 
-	print(string.format("|cff6f6fffErfahrung erhalten: %d (%d verbleibend, noch %dx)", gained, remaining, mobs));
+frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+frame:RegisterEvent("PLAYER_XP_UPDATE");
+
+frame:SetScript("OnEvent", function(self, event, ...)
+	if (event == "PLAYER_ENTERING_WORLD") then
+		cur = UnitXP("player");
+		max = UnitXPMax("player");
+	end
+
+	if (event == "PLAYER_XP_UPDATE") then
+		local new_cur = UnitXP("player");
+		local new_max = UnitXPMax("player");
+		local gained = 0;
+
+		if (new_max > max) then
+			-- special calculation on level up
+			gained = (max - cur) + new_cur;
+		else
+			gained = new_cur - cur;
+		end
+
+		cur = new_cur;
+		max = new_max;
+
+		local remaining = max - cur;
+
+		if (gained > 0 and remaining > 0) then
+			local mobs = math.ceil(remaining / gained);
+			print(string.format("|cff6f6fffErfahrung erhalten: %d (%d verbleibend, noch %dx)", gained, remaining, mobs));
+		end
+	end
 end);
