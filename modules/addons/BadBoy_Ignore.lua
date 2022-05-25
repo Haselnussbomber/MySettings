528f3dce068ba2951d9b2b4c23efb6a8c9664a51
Haselnussbomber <mail@haselnussbomber.de>
2022-05-25 23:40:39 +0200

    add BadBoy_Ignore UnitPopup entry

---
+++ modules/addons/BadBoy_Ignore.lua
@@ -0,0 +1,54 @@
+local _, addon = ...;
+
+local module = addon:NewModule("BadBoy_Ignore", "AceEvent-3.0");
+
+function module:OnInitialize()
+	self:RegisterEvent("ADDON_LOADED");
+end
+
+function module:ADDON_LOADED(_, addonName)
+	if (addonName ~= "BadBoy_Ignore") then
+		return;
+	end
+
+	self:UnregisterEvent("ADDON_LOADED");
+
+	-- Unit Popup Button
+	UnitPopupButtons["BBI"] = {
+		text = function(dropdownMenu)
+			local key = dropdownMenu.name .. "-" .. (dropdownMenu.server or CURRENT_SERVER);
+			return "BadBoy: " .. (BADBOY_IGNORE[key] and IGNORE_REMOVE or IGNORE);
+		end
+	};
+	table.insert(UnitPopupMenus["FRIEND"], #(UnitPopupMenus["FRIEND"])-1, "BBI");
+
+	local CURRENT_NAME, CURRENT_SERVER;
+
+	hooksecurefunc("UnitPopup_ShowMenu", function(self, which)
+		if (which == "FRIEND" and UIDROPDOWNMENU_MENU_LEVEL == 1) then
+			CURRENT_NAME, CURRENT_SERVER = self.name, self.server;
+		end
+	end);
+
+	hooksecurefunc("UnitPopup_OnClick", function(self)
+		local name, server = UIDROPDOWNMENU_INIT_MENU.name, UIDROPDOWNMENU_INIT_MENU.server;
+		if (not server) then
+			return;
+		end
+
+		if (name == CURRENT_NAME and not server) then
+			server = CURRENT_SERVER;
+		end
+
+		if (self.value == "BBI") then
+			local key = name .. "-" .. server;
+			if (BADBOY_IGNORE[key]) then
+				BADBOY_IGNORE[key] = nil;
+				print("|cFF33FF99BadBoy_Ignore:|r Removed " .. GetPlayerLink(key, name) .. " from ignore list");
+			else
+				BADBOY_IGNORE[key] = true;
+				print("|cFF33FF99BadBoy_Ignore:|r Added " .. GetPlayerLink(key, name) .. " to ignore list");
+			end
+		end
+	end);
+end
