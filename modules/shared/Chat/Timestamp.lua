68a9ab64c413ee85f282d451006eb6b828641974
Haselnussbomber <mail@haselnussbomber.de>
2022-05-31 18:28:04 +0200

    fix timestamp by patching ElvUI instead
    In HyperLinkedCPL colored links are replaced, so that | will be \10.
    The problem: \10 is the ASCII control code for a line feed and
    E:StripString() trims it away (^%s+), so my colored, linked timestamp
    at the beginning of the message breaks the message.
    
    The provided ElvUI diff patch changes the HyperLinkedCPL function, so
    that it uses the reserved ASCII control codes "Device Control 1" (\17)
    and "Device Control 2" (\18) instead, so they don't get trimmed.
    
    The patch also includes a fix for mawpower links in chat not having
    tooltips and makes |n available in unit/raid frame texts.
    
    Move .patch file to your ElvUI repo and apply with
    git apply ElvUI-12.79.patch

--- modules/shared/Chat/Timestamp.lua
+++ modules/shared/Chat/Timestamp.lua
@@ -1,35 +1,21 @@
-local _, addon = ...;
-
 local date = date;
+
+local timestampColor = CreateColor(245 / 255, 245 / 255, 245 / 255);
 
 local function AddMessage(chat, text, ...)
 	local id = chat:GetID();
 	if (id and text) then
-		if (IsAddOnLoaded("ElvUI")) then
-			text = ("|cff%02x%02x%02x|Hcpl:%s|h[%s]|h|r %s"):format(245, 245, 245, id, date("%X"), text);
-		else
-			text = ("|cff%02x%02x%02x[%s]|r %s"):format(245, 245, 245, date("%X"), text);
-		end
+		local timestamp = ("|Hcpl:%d|h[%s]|h"):format(id, date("%X"));
+		text = timestampColor:WrapTextInColorCode(timestamp) .. " " .. text;
 	end
 	return chat:OriginalAddMessage(text, ...);
 end
 
--- adds timestamp. clickable to copy text if ElvUI is loaded
-local module = addon:NewModule("ChatTimestamp");
-
-function module:OnInitialize()
-	self:RegisterEvent("PLAYER_ENTERING_WORLD");
-end
-
-function module:PLAYER_ENTERING_WORLD()
-	self:UnregisterEvent("PLAYER_ENTERING_WORLD");
-
-	-- inject timestamp link
-	for i = 1, NUM_CHAT_WINDOWS do
-		local cf = _G["ChatFrame"..i];
-		if (cf ~= COMBATLOG) then
-			cf.OriginalAddMessage = cf.AddMessage;
-			cf.AddMessage = AddMessage;
-		end
+-- inject timestamp link
+for i = 1, NUM_CHAT_WINDOWS do
+	local cf = _G["ChatFrame"..i];
+	if (cf ~= COMBATLOG) then
+		cf.OriginalAddMessage = cf.AddMessage;
+		cf.AddMessage = AddMessage;
 	end
 end
