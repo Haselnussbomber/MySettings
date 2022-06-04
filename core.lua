4d62332d15424e90a74424cf26830bae6e2d13d3
Haselnussbomber <mail@haselnussbomber.de>
2022-06-04 13:46:43 +0200

    reverse moving addon variables into OnInitialize
    happened in commit 2044c879

--- core.lua
+++ core.lua
@@ -4,8 +4,6 @@ MySettings = LibStub("AceAddon-3.0"):NewAddon(addon, addonName);
 
 MySettings:SetDefaultModuleLibraries("AceEvent-3.0");
 
-function MySettings:OnInitialize()
-  self.IsClassic = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC;
-  self.IsBCC = WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC;
-  self.IsMainline = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE;
-end
+addon.IsClassic = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC;
+addon.IsBCC = WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC;
+addon.IsMainline = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE;
