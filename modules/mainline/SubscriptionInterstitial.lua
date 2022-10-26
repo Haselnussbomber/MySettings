2b067f767b671be6b103d619c304d4dab06e5caf
Haselnussbomber <mail@haselnussbomber.de>
2022-10-26 20:57:26 +0200

    shush

---
+++ modules/mainline/SubscriptionInterstitial.lua
@@ -0,0 +1,14 @@
+local _, addon = ...;
+
+local module = addon:NewModule("SubscriptionInterstitial");
+
+function module:OnInitialize()
+	self:RegisterEvent("SHOW_SUBSCRIPTION_INTERSTITIAL");
+end
+
+function module:SHOW_SUBSCRIPTION_INTERSTITIAL()
+  HideUIPanel(SubscriptionInterstitialFrame);
+
+	SubscriptionInterstitialFrame:UnregisterEvent("SHOW_SUBSCRIPTION_INTERSTITIAL");
+	self:UnregisterEvent("SHOW_SUBSCRIPTION_INTERSTITIAL");
+end
