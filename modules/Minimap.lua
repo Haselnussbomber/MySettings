19416a20a5c829255657d9320629921573132bcf
Haselnussbomber <mail@haselnussbomber.de>
2023-07-13 04:27:16 +0200

    modify minimap

---
+++ modules/Minimap.lua
@@ -0,0 +1,24 @@
+local addonName = ...;
+local maskHover = "Interface\\Addons\\"..addonName.."\\media\\minimap-mask";
+local maskDefault = "Interface\\ChatFrame\\ChatFrameBackground";
+
+MinimapCompassTexture:Hide();
+
+Minimap:SetMaskTexture(maskHover);
+
+Minimap:HookScript("OnEnter", function()
+	Minimap:SetMaskTexture(maskDefault);
+end);
+
+Minimap:HookScript("OnLeave", function()
+	Minimap:SetMaskTexture(maskHover);
+end);
+
+-- move up
+MinimapCluster.MinimapContainer.Minimap:ClearAllPoints();
+MinimapCluster.MinimapContainer.Minimap:SetPoint("CENTER", MinimapCluster.MinimapContainer, -6, 22);
+
+-- make this smaller
+ExpansionLandingPageMinimapButton:SetScale(0.8);
+ExpansionLandingPageMinimapButton:ClearAllPoints();
+ExpansionLandingPageMinimapButton:SetPoint("BOTTOMLEFT", MinimapBackdrop, 20, 30);
