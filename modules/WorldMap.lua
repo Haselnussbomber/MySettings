52ffd1de6c9c3fbf430c91fa309fb3065ff50ccd
Haselnussbomber <mail@haselnussbomber.de>
2023-07-18 01:26:52 +0200

    don't make me load the worldmap

--- modules/WorldMap.lua
+++ modules/WorldMap.lua
@@ -1,35 +1,33 @@
-if (not IsAddOnLoaded("Blizzard_WorldMap")) then
-	LoadAddOn("Blizzard_WorldMap");
-end
+EventUtil.ContinueOnAddOnLoaded("Blizzard_WorldMap", function()
+	-- hide tutorial button
+	WorldMapFrame.BorderFrame.Tutorial:Hide();
+	WorldMapFrame.BorderFrame.Tutorial:HookScript("OnShow", WorldMapFrame.BorderFrame.Tutorial.Hide);
 
--- hide tutorial button
-WorldMapFrame.BorderFrame.Tutorial:Hide();
-WorldMapFrame.BorderFrame.Tutorial:HookScript("OnShow", WorldMapFrame.BorderFrame.Tutorial.Hide);
-
--- scale world map
-local scale = 1.15;
-WorldMapFrame:SetScale(scale);
-
--- fix cursor position
-WorldMapFrame.ScrollContainer.GetCursorPosition = function()
-	local currentX, currentY = GetCursorPosition();
-	local effectiveScale = WorldMapFrame:GetEffectiveScale();
-	return currentX / effectiveScale, currentY / effectiveScale;
-end;
-
--- handle minimize/maximize
-hooksecurefunc(WorldMapFrame, "Maximize", function()
-	WorldMapFrame:SetScale(1);
-end);
-
-hooksecurefunc(WorldMapFrame, "Minimize", function()
+	-- scale world map
+	local scale = 1.15;
 	WorldMapFrame:SetScale(scale);
-end);
 
--- hide WorldMapActivityTracker
-for _, frame in next, WorldMapFrame.overlayFrames do
-	if (frame.SetSelectedBounty) then
-		frame.Refresh = frame.Clear;
-		break;
+	-- fix cursor position
+	WorldMapFrame.ScrollContainer.GetCursorPosition = function()
+		local currentX, currentY = GetCursorPosition();
+		local effectiveScale = WorldMapFrame:GetEffectiveScale();
+		return currentX / effectiveScale, currentY / effectiveScale;
+	end;
+
+	-- handle minimize/maximize
+	hooksecurefunc(WorldMapFrame, "Maximize", function()
+		WorldMapFrame:SetScale(1);
+	end);
+
+	hooksecurefunc(WorldMapFrame, "Minimize", function()
+		WorldMapFrame:SetScale(scale);
+	end);
+
+	-- hide WorldMapActivityTracker
+	for _, frame in next, WorldMapFrame.overlayFrames do
+		if (frame.SetSelectedBounty) then
+			frame.Refresh = frame.Clear;
+			break;
+		end
 	end
-end
+end);
