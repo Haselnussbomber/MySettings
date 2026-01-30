92eda3e77c87044c869f0fae8d3e80fec077b805
Haselnussbomber <mail@haselnussbomber.de>
2026-01-31 00:35:32 +0100

    Replace old code with SellAllJunkItems

--- modules/AutoSellAndRepair.lua
+++ modules/AutoSellAndRepair.lua
@@ -7,25 +7,5 @@ MerchantFrame:HookScript("OnShow", function(self)
 		PlaySound(SOUNDKIT.ITEM_REPAIR);
 	end
 
-	-- disabled until next patch
-	-- C_MerchantFrame.SellAllJunkItems();
-
-	-- sell grays (copied from ElvUI)
-	for bagID = 0, 4 do
-		for slotID = 1, C_Container.GetContainerNumSlots(bagID) do
-			local info = C_Container.GetContainerItemInfo(bagID, slotID);
-			if (info) then
-				local itemLink = info.hyperlink;
-				if (itemLink and not info.hasNoValue) then
-					local _, _, rarity, _, _, _, _, _, _, _, itemPrice, classID, _, bindType = C_Item.GetItemInfo(itemLink);
-
-					if (rarity and rarity == 0
-						and (classID ~= 12 or bindType ~= 4) -- Quest can be classID:12 or bindType:4
-						and (not C_TransmogCollection.GetItemInfo(itemLink) or C_TransmogCollection.PlayerHasTransmogByItemInfo(itemLink))) then -- skip transmogable items
-						C_Container.UseContainerItem(bagID, slotID);
-					end
-				end
-			end
-		end
-	end
+	C_MerchantFrame.SellAllJunkItems();
 end);
